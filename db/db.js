f80da51e19a9b084f9f613b503a2ccbfd613ed90
Josh Sandquist <joshdsand@gmail.com>
2023-04-19 12:24:06 -0700

    refactored viewAllEmployee function to include salary

--- db/db.js
+++ db/db.js
@@ -1,12 +1,29 @@
+//requiring connetion variable used for mysql2 login credentials
 const connection = require('./connection')
 
+//Function that should return all Employees and data when called
 function viewAllEmployees(table) {
-    connection.query('SELECT * FROM employee', (err, rows) => {
+    //Refactored the query to include employee salary from the role table in the function
+    connection.query(
+      'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id;',
+      (err, rows) => {
+        if (err) throw err;
+        table(rows);
+      }
+    );
+  }
+
+//Function that should return all departments and data when called
+function viewAllDepartments(table) {
+    connection.query('SELECT * FROM department', (err, rows) => {
       if (err) throw err;
       table(rows);
     });
   }
 
+  //Exporting all functions for use with main index file
   module.exports = {
-    viewAllEmployees: viewAllEmployees
+    viewAllEmployees: viewAllEmployees,
+    viewAllDepartments: viewAllDepartments
   };
+
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const db = require('./db/db');
 db.viewAllEmployees((employees) => {
   console.table(employees);
 });
+
+db.viewAllDepartments((departments) => {
+    console.table(departments);
+  });
+  
