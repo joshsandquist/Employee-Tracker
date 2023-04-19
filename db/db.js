427649a492f5f76b20ed60df687c1544d4cc2158
Josh Sandquist <joshdsand@gmail.com>
2023-04-19 15:41:01 -0700

    added promises to my db.js file

--- db/db.js
+++ db/db.js
@@ -1,41 +1,68 @@
 //requiring connetion variable used for mysql2 login credentials
 const connection = require('./connection')
 
-//Function that should return all Employees and data when called
-function viewAllEmployees(table) {
-    connection.query(
-      `SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, CONCAT(mgr.first_name, ' ', mgr.last_name) AS manager
+//Function that should return all employees and data when called. Joined manager data from the same table to list the manager by name and not id for the employee
+function viewAllEmployees() {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, CONCAT(mgr.first_name, ' ', mgr.last_name) AS manager
       FROM employee AS emp
       LEFT JOIN role ON emp.role_id = role.id
       LEFT JOIN employee AS mgr ON emp.manager_id = mgr.id;`,
-      (err, rows) => {
-        if (err) throw err;
-        table(rows);
-      }
-    );
+       (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
   }
 
 //Function that should return all departments and data when called
-function viewAllDepartments(table) {
-    connection.query('SELECT * FROM department', (err, rows) => {
-      if (err) throw err;
-      table(rows);
+function viewAllDepartments() {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM department', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
     });
   }
 // Function to view all roles. Joined department_name from department table to display name instead of id
-  function viewAllRoles(table) {
-    connection.query(
-      'SELECT role.id, role.title, role.salary, department.name as department_name FROM role LEFT JOIN department ON role.department_id = department.id;',
-      (err, rows) => {
-        if (err) throw err;
-        table(rows);
-      }
-    );
+  function viewAllRoles() {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT role.id, role.title, role.salary, department.name as department_name FROM role LEFT JOIN department ON role.department_id = department.id;',
+       (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
   }
+  
+// Function to add an employee, using the required information.
+    function addEmployee(first_name, last_name, role_id, manager_id) {
+      return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, role_id, manager_id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+
   //Exporting all functions for use with main index file
   module.exports = {
     viewAllEmployees: viewAllEmployees,
     viewAllDepartments: viewAllDepartments,
-    viewAllRoles: viewAllRoles
+    viewAllRoles: viewAllRoles,
+    addEmployee: addEmployee
   };
 
--- index.js
+++ index.js
@@ -1,13 +1,58 @@
+const inquirer = require('inquirer')
 const db = require('./db/db');
 
-db.viewAllEmployees((employees) => {
-  console.table(employees);
-});
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit']}
+        ])
+    }
 
-db.viewAllDepartments((departments) => {
-    console.table(departments);
-  });
+function viewEmployees() {
+  db.viewAllEmployees()
+    .then((rows) => {
+      console.table(rows);
+      mainMenu();
+    })
+    .catch((err) => {
+      console.error(err);
+      mainMenu();
+    });
+}
 
-  db.viewAllRoles((roles) => {
-    console.table(roles);
-  });
+function viewDepartments() {
+    db.viewAllDepartments()
+      .then((rows) => {
+        console.table(rows);
+        mainMenu();
+      })
+      .catch((err) => {
+        console.error(err);
+        mainMenu();
+      });
+  }
+  function viewRoles() {
+    db.viewAllRoles()
+      .then((rows) => {
+        console.table(rows);
+        mainMenu();
+      })
+      .catch((err) => {
+        console.error(err);
+        mainMenu();
+      });
+  }
+
+viewEmployees()
